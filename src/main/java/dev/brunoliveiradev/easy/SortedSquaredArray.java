8477c5ad6c9c1f2408a92c0eee7fb81f151fd6f8
Bruno Oliveira <58921482+brunoliveiradev@users.noreply.github.com>
2023-01-23 23:19:24 -0300

    [UPDATE]: Created a new solution for the SortedSquaredArray challenge!

--- src/main/java/dev/brunoliveiradev/easy/SortedSquaredArray.java
+++ src/main/java/dev/brunoliveiradev/easy/SortedSquaredArray.java
@@ -21,11 +21,11 @@ public class SortedSquaredArray {
     }
 
     // Is not the optimal solution: Time complexity: O(n log n) | Space Complexity: O(n) - where n is lenght of the input array
-    public static int[] sortedSquaredArray(int[] array) {
+    public static int[] sortedSquaredArrayV1(int[] array) {
 
         int[] tempArray = new int[array.length];
 
-        for(int i = 0; i < array.length; i++) {
+        for (int i = 0; i < array.length; i++) {
             tempArray[i] = array[i] * array[i];
         }
 
@@ -33,4 +33,25 @@ public class SortedSquaredArray {
         return tempArray;
     }
 
+    // O(n) time | O(n) space - where n is the length of the input array
+    public static int[] sortedSquaredArray(int[] array) {
+        int[] sortedSquares = new int[array.length];
+
+        int smallestValueIndex = 0;
+        int largestValueIndex = array.length - 1;
+
+        for (int i = array.length - 1; i >= 0; i--) {
+            int smallerValue = array[smallestValueIndex];
+            int largestValue = array[largestValueIndex];
+
+            if (Math.abs(smallerValue) > Math.abs(largestValue)) {
+                sortedSquares[i] = smallerValue * smallerValue;
+                smallestValueIndex++;
+            } else {
+                sortedSquares[i] = largestValue * largestValue;
+                largestValueIndex--;
+            }
+        }
+        return sortedSquares;
+    }
 }
