5f6371733fdebb9759f8d6358c26937a19e888b6
Bruno Oliveira <58921482+brunoliveiradev@users.noreply.github.com>
2023-01-28 11:42:15 -0300

    [UPDATE]: Add one more example at TwoPointers technique

--- src/main/java/dev/brunoliveiradev/techniques/TwoPointers.java
+++ src/main/java/dev/brunoliveiradev/techniques/TwoPointers.java
@@ -1,5 +1,7 @@
 package dev.brunoliveiradev.techniques;
 
+import dev.brunoliveiradev.easy.TwoNumberSum;
+
 /**
  * An extremaly common technique used to solve array and string problems.
  * It involves having two integers variables that both move along an iterable.
@@ -29,6 +31,7 @@ public class TwoPointers {
      * A string is a palindrome if it reads the same forwards as backwards.
      * That means, after reversing it, it is still the same string.
      * For example: "abcdcba", or "racecar".
+     *
      * @param input String
      * @return Return true if a given string is a palindrome, false otherwise.
      */
@@ -37,8 +40,8 @@ public class TwoPointers {
         int left = 0;
         int right = input.length() - 1;
 
-        while(left < right) {
-            if(input.charAt(left) != input.charAt(right)) {
+        while (left < right) {
+            if (input.charAt(left) != input.charAt(right)) {
                 return false;
             }
             left++;
@@ -46,4 +49,40 @@ public class TwoPointers {
         }
         return true;
     }
+
+    /**
+     * Given a sorted array of unique integers and a target integer,
+     * return true if there exists a pair of numbers that sum to target, false otherwise.
+     * Because the array is sorted, we can use two pointers to an O(n) time complexity and
+     * constant space O(1).
+     * If we use brute force to iterate over all pairs of integers this would result at
+     * time complexity of O(n^2).
+     *
+     * @param array     int[]
+     * @param targetSum int
+     * @return boolean - true if exist or false otherwise
+     * @apiNote The reason this alforithm works is that the numbers are sorted
+     * @see TwoNumberSum This is similar to TwoNumberSum.
+     */
+    private static boolean twoNumberSumOfSortedArray(int[] array, int targetSum) {
+        int left = 0;
+        int right = array.length - 1;
+
+        while (left < right) {
+            int currentElemnt = array[left] + array[right];
+
+            if (currentElemnt == targetSum) {
+                return true;
+            }
+
+            if (currentElemnt > targetSum) {
+                right--;
+            } else {
+                left++;
+            }
+        }
+
+        return false;
+    }
+
 }
