b0bcf0512127bde7908491b444ae97f6e2db0008
Bruno Oliveira <58921482+brunoliveiradev@users.noreply.github.com>
2023-02-03 08:36:48 -0300

    [FEATURE]: Upadate with more examples of the SlindingWindow technique

--- src/main/java/dev/brunoliveiradev/techniques/SlidingWindow.java
+++ src/main/java/dev/brunoliveiradev/techniques/SlidingWindow.java
@@ -46,68 +46,115 @@ public class SlidingWindow {
         }
     }
 
-    /**
-     * First example: Given an array of positive integers nums and an integer k,
-     * find the length of the longest subarray whose sum is less than or equal to k.
-     *
-     * @param nums int[] - an argument
-     * @param k    int - another argument
-     * @return subarray whose sum is less than or equal to k.
-     * @apiNote Time Complexity: O(n) | Space Complexity: O(1) - where n is the length of nums
-     */
-    private static int findLength(int[] nums, int k) {
-        // Let's use an integer current that tracks the sum of the current window. current <= k.
-        int current = 0;
-        int left = 0;
-        int answer = 0;
+    static class findingTheLengthOfSubarray {
+        /**
+         * First example: Given an array of positive integers nums and an integer k,
+         * find the length of the longest subarray whose sum is less than or equal to k.
+         *
+         * @param nums int[] - an argument
+         * @param k    int - another argument
+         * @return subarray whose sum is less than or equal to k.
+         * @apiNote Time Complexity: O(n) | Space Complexity: O(1) - where n is the length of nums
+         */
+        public static int findLength(int[] nums, int k) {
+            // Let's use an integer current that tracks the sum of the current window. current <= k.
+            int current = 0;
+            int left = 0;
+            int answer = 0;
 
-        // At each number, will add the number to the current window's sum
-        for (int right = 0; right < nums.length; right++) {
-            current += nums[right];
+            // At each number, will add the number to the current window's sum
+            for (int right = 0; right < nums.length; right++) {
+                current += nums[right];
 
-            // While the constraint is broken, remove the elements from the left and increment
-            while (current > k) {
-                current -= nums[left];
-                left++;
-            }
-            // Updates the answer with the maximum length found.
-            // Given a subarray starting at left and ending at right, the length is right - left + 1
-            answer = Math.max(answer, right - left + 1);
-        }
-
-        return answer;
-    }
-
-    /**
-     * Given a binary substring s, flip a "0"into "1" and return the length of
-     * the longest substring containing only "1" after performing at MOST ONE operation.
-     *
-     * @param s an argument of a binary substring (string containing only "0" and "1")
-     * @return length of the longest
-     * @apiNote Because the string can only contain "1" and "0", another way to look at this problem is
-     * "what is the longest substring that contains at most one "0"?"
-     * @bigO this problem runs in O(n) time, where n is the length of s. Space complexity: O(1).
-     */
-    private static int findAnotherLenght(String s) {
-        int current = 0;
-        int left = 0;
-        int answer = 0;
-
-        for (int right = 0; right < s.length(); right++) {
-            if (s.charAt(right) == '0') {
-                current++;
-            }
-            // While the constraint is broken, remove the elements from the current and increment left
-            while (current > 1) {
-                if (s.charAt(left) == '0') {
-                    current--;
+                // While the constraint is broken, remove the elements from the left and increment
+                while (current > k) {
+                    current -= nums[left];
+                    left++;
                 }
-                left++;
+                // Updates the answer with the maximum length found.
+                // Given a subarray starting at left and ending at right, the length is right - left + 1
+                answer = Math.max(answer, right - left + 1);
             }
-            //lenght of the substring = right - left + 1;
-            answer = Math.max(answer, right - left + 1);
+
+            return answer;
         }
 
-        return answer;
+        /**
+         * Given a binary substring s, flip a "0"into "1" and return the length of
+         * the longest substring containing only "1" after performing at MOST ONE operation.
+         *
+         * @param s an argument of a binary substring (string containing only "0" and "1")
+         * @return length of the longest
+         * @apiNote Because the string can only contain "1" and "0", another way to look at this problem is
+         * "what is the longest substring that contains at most one "0"?"
+         * @bigO this problem runs in O(n) time, where n is the length of s. Space complexity: O(1).
+         */
+        public static int findAnotherLenght(String s) {
+            int current = 0;
+            int left = 0;
+            int answer = 0;
+
+            for (int right = 0; right < s.length(); right++) {
+                if (s.charAt(right) == '0') {
+                    current++;
+                }
+                // While the constraint is broken, remove the elements from the current and increment left
+                while (current > 1) {
+                    if (s.charAt(left) == '0') {
+                        current--;
+                    }
+                    left++;
+                }
+                //lenght of the substring = right - left + 1;
+                answer = Math.max(answer, right - left + 1);
+            }
+
+            return answer;
+        }
     }
+
+    /**
+     * If a problem asks for the number of subarrays that fit some constraint,
+     * we can still use sliding window,
+     * but we need to use a neat math trick to calculate the number of subarrays.
+     *
+     * @apiNote Whenever you see a problem asking for the number of subarray, think of this:
+     * at each index, how many valid subarrays *end* at this index?
+     */
+    static class findingTheNumberOfSubarray {
+
+        /**
+         * Given an array of positive integers nums and an integer k,
+         * return the number of contiguous subarrays where the product of all the elements in
+         * the subarray is strictly less than k.
+         *
+         * @param nums int[] argument
+         * @param k    int argument
+         * @return int - length of subarray that is stricly less than k
+         * @apiNote Time Complexity: O(n) | Space Complexity: O(1) - where n is the length os nums
+         */
+        public static int findNumberSubarray(int[] nums, int k) {
+            //Do a normal sliding window with the constraint of the product being less than k, and at each index, add the length of the window (right - left + 1) to the answer.
+            if (k <= 1) return 0;
+
+            int current = 1; // because current will be used in a multiplication, cant be 0.
+            int left = 0;
+            int answer = 0;
+
+            for (int right = 0; right < nums.length; right++) {
+                current *= nums[right];
+                // While the constraint is broken, remove the elements from the current window
+                while (current >= k) {
+                    current /= nums[left];
+                    left++;
+                }
+
+                answer += right - left + 1;
+            }
+
+            return answer;
+        }
+
+    }
+
 }
