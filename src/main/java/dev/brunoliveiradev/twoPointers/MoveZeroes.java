8661e0ba71711062277fd49d3de9a0ad97cae5f9
Bruno Oliveira <58921482+brunoliveiradev@users.noreply.github.com>
2023-03-09 22:03:59 -0300

    [FEATURE]: Solved the MoveZeroes challenge using Dutch National Flag Algorithm

--- src/main/java/dev/brunoliveiradev/twoPointers/MoveZeroes.java
+++ src/main/java/dev/brunoliveiradev/twoPointers/MoveZeroes.java
@@ -21,7 +21,42 @@ public class MoveZeroes {
         System.out.println("Moved Zeroes to the last position: " + Arrays.toString(array));
     }
 
+    /**
+     * Moves the zeroes to the last positions in the array
+     * @param nums int array
+     * @apiNote Time Complexity: O(n) - where n is the length of the input array nums
+     * | Space Complexity: O(1)
+     * @implNote This approach is also known as "Dutch National Flag Algorithm"
+     * a well-known algorithm for sorting an array of 0s, 1s, and 2s.
+     * It was originally designed by Edsger Dijkstra, a Dutch computer scientist, in the 1970s.
+     */
     public static void moveZeroes(int[] nums) {
+        if (nums.length == 1) return;
+
+        int snowBallSize = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) {
+                // if wee meet 0, the size increase;
+                snowBallSize++;
+            }
+            // swap with the most left zero
+            else if (snowBallSize > 0) {
+                int temp = nums[i];
+                nums[i] = 0;
+                nums[i - snowBallSize] = temp;
+            }
+        }
+    }
+
+    /**
+     * Moves the zeroes to the last positions in the array
+     * @param nums int array
+     * @apiNote Time Complexity: O(n) - where n is the length of the input array nums
+     * | Space Complexity: O(1)
+     * @implNote Use the TwoPointers approach
+     */
+    public static void moveZeroesWithTwoPointers(int[] nums) {
         if (nums.length == 1) return;
 
         int right = 0;
@@ -39,4 +74,5 @@ public class MoveZeroes {
         nums[left] = nums[right];
         nums[right] = temp;
     }
+
 }
