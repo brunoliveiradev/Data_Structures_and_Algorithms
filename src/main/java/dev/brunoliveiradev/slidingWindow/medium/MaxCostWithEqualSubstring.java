fd18454870aab21dd30c558d3d797a29683025f5
Bruno Oliveira <58921482+brunoliveiradev@users.noreply.github.com>
2023-03-17 09:02:24 -0300

    [NEW] First intuition to solve the equalSubstring challenge.

---
+++ src/main/java/dev/brunoliveiradev/slidingWindow/medium/MaxCostWithEqualSubstring.java
@@ -0,0 +1,29 @@
+package dev.brunoliveiradev.slidingWindow.medium;
+
+public class MaxCostWithEqualSubstring {
+
+    public static int equalSubstring(String s, String t, int maxCost) {
+        int costToChange = 0;
+        int currentCost = 0;
+        int maxChange = 0;
+
+        //Since s.length == t.length doesn't matter wich one I choose
+        for (int i = 0; i < s.length(); i++) {
+            costToChange = Math.abs(s.charAt(i) - t.charAt(i));
+
+            if(costToChange <= maxCost){
+                currentCost = maxCost - costToChange;
+            } else {
+                currentCost--;
+            }
+
+            maxChange = Math.max(currentCost, maxChange);
+        }
+        return maxChange;
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+}
