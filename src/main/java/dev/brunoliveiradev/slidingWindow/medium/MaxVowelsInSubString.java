dd79b9b23727b582a4eadd7d85b03184004c476e
Bruno Oliveira <58921482+brunoliveiradev@users.noreply.github.com>
2023-03-16 21:05:47 -0300

    [UPDATE] Updated the MaxVowelsInSubString with a call function inside the main method.

--- src/main/java/dev/brunoliveiradev/slidingWindow/medium/MaxVowelsInSubString.java
+++ src/main/java/dev/brunoliveiradev/slidingWindow/medium/MaxVowelsInSubString.java
@@ -1,7 +1,17 @@
 package dev.brunoliveiradev.slidingWindow.medium;
 
+import java.util.Scanner;
+
 public class MaxVowelsInSubString {
 
+    /**
+     * Given a string s and an integer k, return the maximum number of vowel letters in any substring of s with length k.
+     *
+     * @param s String input that consists of lowercase English letters.
+     * @param k int argument that represent the window size
+     * @return int - Maximum Number of Vowels in a Substring of Given Length
+     * @apiNote Time Complexity: O(n) | Space Complexity: O(1)
+     */
     public static int maxVowels(String s, int k) {
         int vowel = 0;
         int maxVowels = 0;
@@ -11,7 +21,8 @@ public class MaxVowelsInSubString {
             if (isVowel(s.charAt(i))) {
                 vowel++;
             }
-            // if constraint is broken, delete the char of the previous window
+            // if constraint is broken (i >= k), the function checks if the character at the left end of the window is a vowel
+            // If so, the variable vowel is decremented by one to account for the removal of that character from the window.
             if (i >= k && isVowel(s.charAt(i - k))) {
                 vowel--;
             }
@@ -28,6 +39,15 @@ public class MaxVowelsInSubString {
     }
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Enter a string: ");
+        String s = scanner.nextLine();
+
+        System.out.print("Enter a window size: ");
+        int k = scanner.nextInt();
+
+        int max = maxVowels(s, k);
+        System.out.println("Max number of vowels in any window of size " + k + " is " + max);
     }
 }
