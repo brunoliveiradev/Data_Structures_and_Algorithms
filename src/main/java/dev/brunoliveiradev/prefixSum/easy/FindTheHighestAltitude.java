b157361f54da7e05d139d9c3bc36fc4b4702fe82
Bruno Oliveira <58921482+brunoliveiradev@users.noreply.github.com>
2023-03-28 08:52:13 -0300

    [NEW] Created the FindTheHighestAltitude challenge.

---
+++ src/main/java/dev/brunoliveiradev/prefixSum/easy/FindTheHighestAltitude.java
@@ -0,0 +1,21 @@
+package dev.brunoliveiradev.prefixSum.easy;
+
+public class FindTheHighestAltitude {
+
+    /**
+     * There is a biker going on a road trip. The road trip consists of n + 1 points at different altitudes.
+     * The biker starts his trip on point 0 with altitude equal 0.
+     * <p>
+     * You are given an integer array gain of length n where gain[i] is the net gain in altitude
+     * between points i and i + 1 for all (0 <= i < n).
+     * <p>
+     * Return the highest altitude of a point.
+     *
+     * @param gain int argument
+     * @return int - the highest altitude of a point
+     */
+    public int largestAltitude(int[] gain) {
+
+        return 0;
+    }
+}
