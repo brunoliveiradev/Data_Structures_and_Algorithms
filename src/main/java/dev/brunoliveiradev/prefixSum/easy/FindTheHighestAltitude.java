7997f9569daec4345a7fc8ee9ade90ec506f462e
Bruno Oliveira <58921482+brunoliveiradev@users.noreply.github.com>
2023-03-29 21:33:37 -0300

    [FEATURE] Solved the FindTheHighestAltitude challenge.

--- src/main/java/dev/brunoliveiradev/prefixSum/easy/FindTheHighestAltitude.java
+++ src/main/java/dev/brunoliveiradev/prefixSum/easy/FindTheHighestAltitude.java
@@ -1,5 +1,7 @@
 package dev.brunoliveiradev.prefixSum.easy;
 
+import java.util.Scanner;
+
 public class FindTheHighestAltitude {
 
     /**
@@ -13,9 +15,34 @@ public class FindTheHighestAltitude {
      *
      * @param gain int argument
      * @return int - the highest altitude of a point
+     * @apiNote Time and Space Complexity: O(N) - where N is the length of the input array
+     * @implNote The highest altitude is the maximum value of the current altitude. Used prefix sum approach to solve.
      */
-    public int largestAltitude(int[] gain) {
+    public static int largestAltitude(int[] gain) {
+        int highestAltitude = 0;
+        int currentAltitude = 0;
 
-        return 0;
+        for (int altitudeGain : gain) {
+            currentAltitude += altitudeGain;
+            highestAltitude = Math.max(highestAltitude, currentAltitude);
+        }
+
+        return highestAltitude;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the sequence of altitude gains separated by space: ");
+        String input = scanner.nextLine();
+
+        String[] gainStringArray = input.split(" ");
+        int[] gain = new int[gainStringArray.length];
+        for (int i = 0; i < gainStringArray.length; i++) {
+            gain[i] = Integer.parseInt(gainStringArray[i]);
+        }
+
+        int highestAltitude = largestAltitude(gain);
+
+        System.out.println("The highest altitude reached is" + highestAltitude);
     }
 }
