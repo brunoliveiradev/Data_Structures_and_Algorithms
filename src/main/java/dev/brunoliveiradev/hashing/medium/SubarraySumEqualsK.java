c71088f1c9f9eaa1db33b23ed2eb233554ad5ecb
Bruno Oliveira <58921482+brunoliveiradev@users.noreply.github.com>
2023-05-04 21:15:51 -0300

    [FEATURE] Add time and space complexity to the SubarraySumEqualsK Challenge.

--- src/main/java/dev/brunoliveiradev/hashing/medium/SubarraySumEqualsK.java
+++ src/main/java/dev/brunoliveiradev/hashing/medium/SubarraySumEqualsK.java
@@ -22,14 +22,20 @@ public class SubarraySumEqualsK {
      * @param nums array of integers non-empty
      * @param k    integer
      * @return the total number of continuous subarrays whose sum equals to k
+     * @apiNote Time complexity O(n) | Space complexity O(n) - where n is the length of the input array.
+     * @implNote We use a hash map to store the current sum and its frequency.
      */
     public static int subarraySum(int[] nums, int k) {
         Map<Integer, Integer> counts = new HashMap<>();
         counts.put(0, 1); // this is the base case, the empty subarray (sum = 0)
 
-        int currSum = 0;
+        int currSum = 0; // track the prefix sum
         int answer = 0;
 
+        // Because the array can have negative numbers, the same prefix can occur multiple times.
+        // We use a hash map counts to track the number of times the prefix sum has occurred.
+        // For every element in the array, we check if the complement (currSum - k) exists in the hash map.
+        // If it exists, we add the frequency of the complement to the answer.
         for (int number : nums) {
             currSum += number;
 
